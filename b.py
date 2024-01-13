a21a068284b93fdd2f55de392e4bb277016b70be
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2024-01-13 19:46:24 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,2 +1,9 @@
-import X64
-X64.Spy()
+import os,time,platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import X64
+    X64.Spy()
+elif bit=='32bit':
+    import X32
+    X32.Spy()
