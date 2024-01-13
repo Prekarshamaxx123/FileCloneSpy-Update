ddd590b93a12308b33f30fb8db2acca5b3659a84
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2024-01-13 18:59:49 +0530

    Update PRE_SPY.py

--- PRE_SPY.py
+++ PRE_SPY.py
@@ -2,10 +2,6 @@ import os,time,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import X64
     import SVR
-    X64.Spy()
 elif bit=='32bit':
-    import X32
-    import SVR
-    X32.Spy()
+    import SVR32
