c9d9416cef165bb68327ee0d86c176b520b1170c
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2024-01-25 08:07:15 +0530

    Delete PRE_SPY.py

--- PRE_SPY.py
+++
@@ -1,9 +0,0 @@
-import os,time,platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    import PSPY
-    PSPY.Spy()
-elif bit=='32bit':
-    import X32
-    X32.Spy()
