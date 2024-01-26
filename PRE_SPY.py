95ea14352c451a1b1baae0f321511b4a7821244b
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2024-01-26 18:50:24 +0530

    Update PRE_SPY.py

--- PRE_SPY.py
+++ PRE_SPY.py
@@ -2,8 +2,8 @@ import os,time,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import PSPY
-    PSPY.Spy()
+    import X64
+    X64.Spy()
 elif bit=='32bit':
     import X32
     X32.Spy()
